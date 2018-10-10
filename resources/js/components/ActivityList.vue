526de385c1270e401177b3ed4b985ba9356b40d5
ferdie <ferdiebergado@gmail.com>
2018-10-10 12:53:15 +0800

    Cleaned activity list

--- resources/js/components/ActivityList.vue
+++ resources/js/components/ActivityList.vue
@@ -57,8 +57,7 @@ export default {
       activityindex: "",
       activity: "",
       role: "",
-      disabled: true,
-      forremoval: {}
+      disabled: true
     };
   },
   mounted() {
@@ -116,10 +115,24 @@ export default {
     removeItem(index) {
       var vm = this;
       let a = vm.$data.activities;
-      console.log(a);
-      console.log(index);
       a.splice(index, 1);
     }
   }
 };
 </script>
+
+<style scoped>
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
