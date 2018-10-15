221f2a3ba3cf9d9e54e480259c8a91e08b80075b
ferdie <ferdiebergado@gmail.com>
2018-10-15 15:36:51 +0800

    Pushed static assets

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-10-03 09:13:32.
+    * Generated on 2018-10-07 08:00:42.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
