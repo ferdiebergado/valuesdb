f648f00f91577d51929ab350f8b92096ac332d12
ferdie <ferdiebergado@gmail.com>
2018-10-16 18:03:32 +0800

    Passed default region to event emitter

--- resources/js/components/RegionSelect.vue
+++ resources/js/components/RegionSelect.vue
@@ -1,37 +1,40 @@
 <template>
     <select name="region_id" id="region_id" class="form-control" v-model.number="region" @change="updateDivisions">
-        <option value="">Select Region</option>
+        <option value="">Please select a Region...</option>
         <option v-for="region in regions" :key="region.id" :value="region.id">{{ region.name }}</option>
     </select>
 </template>
 <script>
 export default {
-    props: {
-        regionid: String
+  props: {
+    regionid: String
+  },
+  data() {
+    return {
+      region: this.regionid,
+      regions: {}
+    };
+  },
+  mounted() {
+    this.fetchRegions();
+    this.updateDivisions();
+  },
+  methods: {
+    fetchRegions() {
+      var vm = this;
+      axios
+        .get("/values/regions")
+        .then(res => {
+          vm.$set(vm.$data, "regions", res.data);
+        })
+        .catch(err => {
+          alert(err.response.data);
+          console.log(err.response.data);
+        });
     },
-    data() {
-        return {
-            region: this.regionid,
-            regions: {}
-        }
-    },
-    mounted() {
-        this.fetchRegions();
-        this.updateDivisions();
-    },
-    methods: {
-        fetchRegions() {
-            var vm = this;
-            axios.get('/values/regions').then(res => {
-                vm.$set(vm.$data, 'regions', res.data);
-            }).catch(err => {
-                alert(err.response.data);
-                console.log(err.response.data);
-            });
-        },
-        updateDivisions() {
-            eventBus.$emit('region-updated', this.region);
-        }
+    updateDivisions() {
+      eventBus.$emit("region-updated", this.region, this.regionid);
     }
-}
+  }
+};
 </script>
